cc9c5272822da5a158ade89dbb5d1f010092ca3f
rinaldi <rinalfauzi@gmail.com>
2022-11-10 07:31:28 +0700

    set master data

---
+++ database/migrations/2022_11_10_001015_create_achivements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achivements', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('nama');
+            $table->time('time_from');
+            $table->time('time_to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achivements');
+    }
+};
---
+++ database/migrations/2022_11_10_002641_create_plannings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plannings', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->integer('qty_target');
+            $table->decimal('waktu_target');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plannings');
+    }
+};
