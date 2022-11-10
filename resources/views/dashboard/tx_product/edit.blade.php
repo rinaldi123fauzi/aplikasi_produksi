52c0da5dfd2e08bdde9e9a29a213b69aca28eae9
rinaldi <rinalfauzi@gmail.com>
2022-11-11 05:22:10 +0700

    perbaikan halaman index lokasi dan form edit transaksi produk

--- resources/views/dashboard/tx_product/edit.blade.php
+++ resources/views/dashboard/tx_product/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Tambah Transaksi Produk</h1>
+        <h1 class="h2">Edit Transaksi Produk</h1>
     </div>
     <div class="col-lg-8">
         <form method="post" action="/tx_product/{{ $productTransaction->id }}">
